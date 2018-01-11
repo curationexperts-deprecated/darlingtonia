0a8137d8846e7de77569cebf3ccc6db22d658d26
Tom Johnson <tom@dp.la>
2018-01-11 15:56:58 -0800

    Build out CSV format validators
    This validator checks that a CSV file is properly formatted. It uses
    MalformedCSVError to supply relevant information to the user.
    
    See: http://ruby-doc.org/stdlib-2.0.0/libdoc/csv/rdoc/CSV/MalformedCSVError.html

--- spec/darlingtonia/csv_format_validator_spec.rb
+++ spec/darlingtonia/csv_format_validator_spec.rb
@@ -3,5 +3,36 @@
 require 'spec_helper'
 
 describe Darlingtonia::CsvFormatValidator do
-  it_behaves_like 'a Darlingtonia::Validator'
+  subject(:validator)  { described_class.new }
+  let(:invalid_parser) { Darlingtonia::CsvParser.new(file: invalid_file) }
+  let(:invalid_file)   { File.open('spec/fixtures/bad_example.csv') }
+
+  it_behaves_like 'a Darlingtonia::Validator' do
+    let(:valid_parser)   { Darlingtonia::CsvParser.new(file: valid_file) }
+    let(:valid_file)     { File.open('spec/fixtures/example.csv') }
+  end
+
+  define :a_validator_error do
+    match do |error|
+      return false unless error.respond_to?(:validator)
+
+      if fields
+        return false if fields[:validator] && error.validator != fields[:validator]
+        return false if fields[:name]      && error.name      != fields[:name]
+      end
+
+      true
+    end
+
+    chain :with, :fields
+  end
+
+  describe '#validate' do
+    it 'returns a Validator::Error' do
+      expect(validator.validate(parser: invalid_parser))
+        .to contain_exactly a_validator_error
+        .with(validator: validator.class,
+              name:      CSV::MalformedCSVError)
+    end
+  end
 end
