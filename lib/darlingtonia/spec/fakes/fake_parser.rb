dc8a9ac6961a8b24bfdb06301e4bd3a3d53c0aed
val99erie <val99erie@gmail.com>
2019-02-01 11:02:46 -0600

    Upgrade version of bixby

--- lib/darlingtonia/spec/fakes/fake_parser.rb
+++ lib/darlingtonia/spec/fakes/fake_parser.rb
@@ -14,11 +14,9 @@ class FakeParser < Darlingtonia::Parser
   end
 end
 
-# rubocop:disable RSpec/FilePath
 describe FakeParser do
   it_behaves_like 'a Darlingtonia::Parser' do
     subject(:parser)   { described_class.new }
     let(:record_count) { 3 }
   end
 end
-# rubocop:enable RSpec/FilePath
