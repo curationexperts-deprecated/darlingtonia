d65641022ad1a8d9a9d15a3141a06b9d50cbcdb3
Jamie Little <jamie@jamielittle.org>
2019-02-11 18:00:38 +0000

    Add a default LogStream class
    This adds a class that will be used by Darlingtonia
    for logging instead of defaulting to STDOUT.
    
    Connected to curationexperts/tenejo#67

---
+++ lib/darlingtonia/log_stream.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Darlingtonia
+  class LogStream
+    ##
+    # @!attribute [rw] logger
+    #   @return [Logger]
+    # @!attribute [rw] severity
+    #   @return [Logger::Serverity]
+    attr_accessor :logger, :severity
+
+    def initialize(logger: nil, severity: nil)
+      self.logger   = logger   || Logger.new(build_filename)
+      self.severity = severity || Logger::INFO
+    end
+
+    def <<(msg)
+      logger.add(severity, msg)
+      STDOUT << msg
+    end
+
+    private
+
+      def build_filename
+        return ENV['IMPORT_LOG'] if ENV['IMPORT_LOG']
+        return rails_log_name if rails_log_name
+        return './darlingtonia_import.log' unless rails_log_name
+      end
+
+      def rails_log_name
+        case Rails.env
+        when 'production'
+          Rails.root.join('log', "csv_import.log").to_s
+        when 'development'
+          Rails.root.join('log', "dev_csv_import.log").to_s
+        when 'test'
+          Rails.root.join('log', "test_csv_import.log").to_s
+        end
+      rescue NameError
+        false
+      end
+  end
+end
