2b21f7b58d3247d7560e8435f8fc15f54cd5467c
Bess Sadler <bess@curationexperts.com>
2019-03-04 09:08:26 -0500

    Importer should respect log settings
    Allow the importer to receive log settings from elsewhere, so it can log
    success messages with all the other logs.
    
    Add elapsed_time and elapsed_time_per_record to logs to help us do
    performance testing.

--- lib/darlingtonia/importer.rb
+++ lib/darlingtonia/importer.rb
@@ -31,10 +31,11 @@ module Darlingtonia
     #   records.
     # @param record_importer [RecordImporter] An object to handle import of
     #   each record
-    def initialize(parser:, record_importer: RecordImporter.new)
+    def initialize(parser:, record_importer: RecordImporter.new, info_stream: Darlingtonia.config.default_info_stream, error_stream: Darlingtonia.config.default_error_stream)
       self.parser          = parser
       self.record_importer = record_importer
-      @info_stream = Darlingtonia.config.default_info_stream
+      @info_stream = info_stream
+      @error_stream = error_stream
     end
 
     ##
@@ -42,8 +43,12 @@ module Darlingtonia
     #
     # @return [void]
     def import
+      start_time = Time.zone.now
+      @info_stream << "event: start_import, batch_id: #{record_importer.batch_id}, expecting to import #{records.count} records."
       records.each { |record| record_importer.import(record: record) }
-      @info_stream << "event: finish_import, batch_id: #{record_importer.batch_id}, successful_record_count: #{record_importer.success_count}, failed_record_count: #{record_importer.failure_count}"
+      end_time = Time.zone.now
+      elapsed_time = end_time - start_time
+      @info_stream << "event: finish_import, batch_id: #{record_importer.batch_id}, successful_record_count: #{record_importer.success_count}, failed_record_count: #{record_importer.failure_count}, elapsed_time: #{elapsed_time}, elapsed_time_per_record: #{elapsed_time/records.count}"
     end
   end
 end
